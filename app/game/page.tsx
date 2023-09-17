4ebb53048d9c8a127509564303693bf407b74157
lansolo99 <lansolo99@hotmail.fr>
2023-09-17 09:35:46 +0200

    Remove home content and set /game redirect

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -9,14 +9,14 @@ const game = (props: Props) => {
   const game1 = gamesData[0];
   const { cards } = game1;
   return (
-    <div className="container mx-auto bg-red-500 flex p-10 flex-col justify-center items-center">
+    <main className="container mx-auto bg-red-500 flex p-10 flex-col justify-center items-center">
       <p>game 1</p>
       <div className="flex flex-col gap-4 max-w-xs mt-6">
         {cards.map((card, i) => {
           return <Card key={`card-${i}`} data={card} />;
         })}
       </div>
-    </div>
+    </main>
   );
 };
 
