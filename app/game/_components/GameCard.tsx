ef8c3ad2a0254898cf94f502018938a691245d45
lansolo99 <lansolo99@hotmail.fr>
2023-09-17 15:43:21 +0200

    Improve card drag #WIP2

--- app/game/_components/GameCard.tsx
+++ app/game/_components/GameCard.tsx
@@ -22,26 +22,19 @@ const GameCard = ({ id, data }: Props) => {
 
   const xInput = [-200, 0, 400];
 
-  const yOutput = [200, 0, 200];
+  const yOutput = [100, 0, 100];
   const rotateOutput = [-20, 0, 20];
 
   let y = useTransform(x, xInput, yOutput);
   let rotation = useTransform(x, xInput, rotateOutput);
 
   return (
-    <motion.div
-      id={`cardWrapper-${id}`}
-      className="absolute bg-white p-4 shadow-lg rounded-lg text-center w-full aspect-[100/150] hover:cursor-grab active:cursor-grab text-black origin-bottom"
-      drag="x"
-      // whileDrag={{ scale: 1.015 }}
-      // whileTap={{ scale: 1.015 }}
-      dragSnapToOrigin
-      dragElastic={0.2}
-      dragConstraints={{ left: 0, right: 0 }}
-      dragTransition={{ bounceStiffness: 1000, bounceDamping: 50 }}
-      style={{ x, y, rotate: rotation }}
-    >
-      <div id="innerCardWrapper">
+    <>
+      <motion.div
+        id={`cardDrivenWrapper-${id}`}
+        className="absolute bg-white p-4 shadow-lg rounded-lg text-center w-full aspect-[100/150] pointer-events-none   text-black origin-bottom"
+        style={{ y, rotate: rotation }}
+      >
         <div id="metrics" className="flex w-full  justify-between">
           <div>1/10</div>
           <div>1</div>
@@ -53,8 +46,19 @@ const GameCard = ({ id, data }: Props) => {
         <p id="affirmation" className="mt-6">
           {affirmation}
         </p>
-      </div>
-    </motion.div>
+      </motion.div>
+
+      <motion.div
+        id={`cardDriverWrapper-${id}`}
+        className="absolute bg-blue-500/0 w-full aspect-[100/150] hover:cursor-grab active:cursor-grab select-none"
+        drag="x"
+        dragSnapToOrigin
+        dragElastic={0.25}
+        dragConstraints={{ left: -100, right: 100 }}
+        dragTransition={{ bounceStiffness: 1000, bounceDamping: 50 }}
+        style={{ x }}
+      ></motion.div>
+    </>
   );
 };
 
