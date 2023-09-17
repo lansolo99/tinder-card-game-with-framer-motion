b89ffffbff07b58b273958dc3b705b9b83cf5d23
lansolo99 <lansolo99@hotmail.fr>
2023-09-17 15:33:40 +0200

    Improve card drag #WIP1

--- app/game/_components/GameCard.tsx
+++ app/game/_components/GameCard.tsx
@@ -4,6 +4,7 @@ import {
   useMotionValue,
   useTransform,
   cubicBezier,
+  useMotionValueEvent,
   circOut,
 } from "framer-motion";
 import { card } from "@/types/games.type";
@@ -15,34 +16,44 @@ type Props = {
 
 const GameCard = ({ id, data }: Props) => {
   const { affirmation } = data;
-  const range = 400;
   const x = useMotionValue(0);
-  let y = useTransform(x, [0, range], [0, 20], { ease: circOut });
-  let rotation = useTransform(x, [0, range], [0, 10], { ease: circOut });
+
+  useMotionValueEvent(x, "change", (latest) => console.log(`x:${latest}`));
+
+  const xInput = [-200, 0, 400];
+
+  const yOutput = [200, 0, 200];
+  const rotateOutput = [-20, 0, 20];
+
+  let y = useTransform(x, xInput, yOutput);
+  let rotation = useTransform(x, xInput, rotateOutput);
 
   return (
     <motion.div
       id={`cardWrapper-${id}`}
-      className="absolute bg-white p-4 shadow-lg rounded-lg text-center w-full aspect-[100/150] hover:cursor-grab active:cursor-grab text-black "
+      className="absolute bg-white p-4 shadow-lg rounded-lg text-center w-full aspect-[100/150] hover:cursor-grab active:cursor-grab text-black origin-bottom"
       drag="x"
       // whileDrag={{ scale: 1.015 }}
       // whileTap={{ scale: 1.015 }}
       dragSnapToOrigin
+      dragElastic={0.2}
+      dragConstraints={{ left: 0, right: 0 }}
       dragTransition={{ bounceStiffness: 1000, bounceDamping: 50 }}
       style={{ x, y, rotate: rotation }}
-      // style={{ x, y }}
     >
-      <div id="metrics" className="flex w-full  justify-between">
-        <div>1/10</div>
-        <div>1</div>
+      <div id="innerCardWrapper">
+        <div id="metrics" className="flex w-full  justify-between">
+          <div>1/10</div>
+          <div>1</div>
+        </div>
+        <div
+          id="illustration"
+          className="w-2/3 mx-auto max-w-[200px] aspect-square rounded-full bg-green-500  mt-4"
+        ></div>
+        <p id="affirmation" className="mt-6">
+          {affirmation}
+        </p>
       </div>
-      <div
-        id="illustration"
-        className="w-2/3 mx-auto max-w-[200px] aspect-square rounded-full bg-green-500  mt-4"
-      ></div>
-      <p id="affirmation" className="mt-6">
-        {affirmation}
-      </p>
     </motion.div>
   );
 };
